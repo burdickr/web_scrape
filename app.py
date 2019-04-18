67e6a96b27ed502c0cffe052415a7c819eccc6a0
Ryan <ryanburdick14@gmail.com>
2019-04-17 19:21:39 -0500

    it actually works!

--- app.py
+++ app.py
@@ -11,20 +11,27 @@ mongo = PyMongo(app)
 # Or set inline
 # mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
 
-
 @app.route("/")
-def index():
-    mars_info = mongo.db.mars_info.find_one()
-    return render_template("index.html", mars_info=mars_info)
+def home():
+
+    # Find one record of data from the mongo database
+    mars_info = mongo.db.collection.find_one()
+
+    # Return template and data
+    return render_template("index.html", mars=mars_info)
 
 
+# Route that will trigger the scrape function
 @app.route("/scrape")
-def scraper():
-    mars_info = mongo.db.mars_info
-    mars_data = scrape_mars.scrape()
-    mars_info.update({}, mars_data, upsert=True)
-    return redirect("/", code=302)
+def scrape():
 
+   # Run the scrape function
+   mars_data = scrape_mars.scrape()
 
+   # Update the Mongo database using update and upsert=True
+   mongo.db.collection.update({}, mars_data, upsert=True)
+
+   # Redirect back to home page
+   return redirect("/")
 if __name__ == "__main__":
-    app.run(debug=True)
+   app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -17,34 +17,31 @@ def init_browser():
 def scrape():
     # returns title and paragraph for news 
     browser = init_browser()
-    news = {}
-    weather = {}
-    space_facts = {}
     url = "https://mars.nasa.gov/news/"
     browser.visit(url)
     html = browser.html
     soup = BeautifulSoup(html, "html.parser")
-    news["news_title"] = soup.find("div", class_='content_title').get_text()
-    news["news_p"] = soup.find("div", class_='article_teaser_body').get_text()
+    news_title = soup.find("div", class_='content_title').get_text()
+    news_body = soup.find("div", class_='article_teaser_body').get_text()
     # featured image
     url2 = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
     browser.visit(url2)
     html = browser.html
     soup = BeautifulSoup(html, "html.parser")
     featured_image = soup.find_all('img')[3]["src"]
-    featured_image_url = {"https://www.jpl.nasa.gov" + featured_image}
+    featured_image_url = ("https://www.jpl.nasa.gov" + featured_image)
     # mars weather
     url3 = "https://twitter.com/marswxreport?lang=en"
     browser.visit(url3)
     html = browser.html
     soup = BeautifulSoup(html, "html.parser")
-    weather["mars_weather"] = soup.find("p", class_='tweet-text').get_text()
+    weather = soup.find("p", class_='tweet-text').get_text()
     # returns table from space facts 
     url4 = "https://space-facts.com/mars/"
     browser.visit(url4)
     html = browser.html
     soup = BeautifulSoup(html, "html.parser")
-    space_facts["table"] = soup.find("tbody").get_text()
+    space_facts = soup.find("tbody").get_text()
     url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
     # hemisphere info 
     url5 = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
@@ -66,5 +63,11 @@ def scrape():
         img_url = list_.a['href']
         image_urls.append(img_url)
         browser.back()
-    hemisphere_image_urls = dict(zip(hemispheres, image_urls))
-    return news, featured_image_url, weather, space_facts, hemisphere_image_urls
+    hemisphere_image_urls = (hemispheres, image_urls)
+    mars_data =  {"news_title": news_title,
+             "news_body": news_body,
+             "featured_image_url": featured_image_url,
+             "weather": weather,
+             "space_facts": space_facts,
+             "hemisphere_image_urls": hemisphere_image_urls}
+    return mars_data
